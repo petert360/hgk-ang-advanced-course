9e3c49318b4e4f759d90aa9279150a7d17e87cc5
petert360 <peter.t360@hotmail.com>
2021-07-26 18:55:53 +0200

    config.service és oldalak létrehozása

--- chapter01-auth/src/app/app.module.ts
+++ chapter01-auth/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './page/nav/nav.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
+import { UsersComponent } from './page/users/users.component';
+import { UserEditComponent } from './page/user-edit/user-edit.component';
+import { ForbiddenComponent } from './page/forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    LoginComponent,
+    UsersComponent,
+    UserEditComponent,
+    ForbiddenComponent
   ],
   imports: [
     BrowserModule,

ab7910cd1f83a9c6fb8a1f58397b8a475b315a28
petert360 <73585032+petert360@users.noreply.github.com>
2021-07-27 21:38:56 +0200

    config, nav, routes

--- angular-feladat01/src/app/app-routing.module.ts
+++ angular-feladat01/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './page/admin/admin.component';
+import { EditorComponent } from './page/editor/editor.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+  },
+  {
+    path: 'editor',
+    component: EditorComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-feladat01/src/app/page/nav/nav.component.ts
+++ angular-feladat01/src/app/page/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/app/service/config.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private config: ConfigService,
+  ) { }
 
   ngOnInit(): void {
   }

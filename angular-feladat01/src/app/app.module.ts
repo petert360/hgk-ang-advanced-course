91f3746387f2d0bb14a040ccaeadc1d69a44ade6
petert360 <73585032+petert360@users.noreply.github.com>
2021-07-27 21:30:16 +0200

    feladat01: komponensek létrehozása

--- angular-feladat01/src/app/app.module.ts
+++ angular-feladat01/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { EditorComponent } from './page/editor/editor.component';
+import { AdminComponent } from './page/admin/admin.component';
+import { LoginComponent } from './page/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EditorComponent,
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

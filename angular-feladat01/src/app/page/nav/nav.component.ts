32ad2ace006ad4028f79a5fcd9dc1373b286111c
petert360 <73585032+petert360@users.noreply.github.com>
2021-07-27 22:37:20 +0200

    NavComponent bővítés

--- angular-feladat01/src/app/page/nav/nav.component.ts
+++ angular-feladat01/src/app/page/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/model/user';
+import { AuthService } from 'src/app/service/auth.service';
 import { ConfigService } from 'src/app/service/config.service';
 
 @Component({
@@ -9,12 +12,26 @@ import { ConfigService } from 'src/app/service/config.service';
 export class NavComponent implements OnInit {
 
   navigation = this.config.navigation;
+  loginStatus = false;
+  userSub: Subscription;
+  user: User | null = null;
 
   constructor(
     private config: ConfigService,
+    private auth: AuthService,
   ) { }
 
   ngOnInit(): void {
+    this.userSub = this.auth.currentUserSubject.subscribe(
+      user => this.user = user
+    );
   }
 
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+
+  onLogout() {
+    this.auth.logout();
+  }
 }

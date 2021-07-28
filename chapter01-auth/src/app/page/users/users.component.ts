6ad5eae9c551efd0a76b0b06fb78c7cbc0cdb66e
petert360 <peter.t360@hotmail.com>
2021-07-28 13:07:16 +0200

    Felhasználók listázása

--- chapter01-auth/src/app/page/users/users.component.ts
+++ chapter01-auth/src/app/page/users/users.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/model/user';
+import { ConfigService } from 'src/app/service/config.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  // list$: Observable<User | User[]> = this.userService.get();
+  //list$: Observable<User | User[]> = (this.userService.get() as unknown as Observable<User[]>);
+  list$: Observable<User[]> = (this.userService.get() as unknown as Observable<User[]>);
+  cols: any[] = this.config.userColumns;
+
+  constructor(
+    public userService: UserService,
+    public config: ConfigService,
+
+  ) { }
 
   ngOnInit(): void {
   }
 
+  update(user: User): void {
+    this.userService.update(user).toPromise().then(
+      userResponse => console.log(userResponse),
+      err => console.error(err)
+    );
+  }
 }
